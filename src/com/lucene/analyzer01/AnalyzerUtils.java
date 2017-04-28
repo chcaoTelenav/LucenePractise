f1c26c9d8f35236fe533de1cbf8a08f03a8584e4
Cao <chcao@china.telenav.com>
2017-04-28 17:24:20 +0800

    new codes added

---
+++ src/com/lucene/analyzer01/AnalyzerUtils.java
@@ -0,0 +1,57 @@
+package com.lucene.analyzer01;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * Created by chcao on 4/28/2017.
+ */
+public class AnalyzerUtils {
+
+	public static void displayToken(String str, Analyzer analyzer) {
+		try {
+			// 首先我们使用分词器analyzer将相关数据（这里比如是内容gcontent）进行分词，这样得到一个词汇流
+			// 然后我们给这个流做一个标记，可以用来遍历此流
+			TokenStream stream = analyzer.tokenStream("content", new StringReader(str));// 这就是一个词汇流
+			CharTermAttribute cta = stream.addAttribute(CharTermAttribute.class);// 相当于一个标记，随着流增加
+
+			stream.reset();
+
+			while (stream.incrementToken()) {
+				System.out.print("[" + cta + "]");
+			}
+			System.out.println();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void displayAllTokenInfo(String str, Analyzer analyzer) {
+		try {
+			TokenStream stream = analyzer.tokenStream("content", new StringReader(str));
+			PositionIncrementAttribute pia = stream.addAttribute(PositionIncrementAttribute.class);
+			OffsetAttribute oa = stream.addAttribute(OffsetAttribute.class);
+			CharTermAttribute cta = stream.addAttribute(CharTermAttribute.class);
+			TypeAttribute ta = stream.addAttribute(TypeAttribute.class);
+
+			stream.reset();
+
+			while (stream.incrementToken()) {
+				System.out.print("位置增量： " + pia.getPositionIncrement());// 词与词之间的空格
+				System.out.print("，单词： " + cta + "[" + oa.startOffset() + "," + oa.endOffset() + "]");
+				System.out.print("，类型： " + ta.type());
+				System.out.println();
+			}
+			System.out.println();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/com/lucene/analyzer01/TestAnalyzer.java
@@ -0,0 +1,58 @@
+package com.lucene.analyzer01;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.util.Version;
+import org.junit.Test;
+
+/**
+ * Created by chcao on 4/28/2017.
+ */
+public class TestAnalyzer {
+
+	@Test
+	public void test01() {
+		Analyzer analyzer1 = new StandardAnalyzer(Version.LUCENE_45);// 标准分词器
+		Analyzer analyzer2 = new StopAnalyzer(Version.LUCENE_45);// 停用分词器
+		Analyzer analyzer3 = new SimpleAnalyzer(Version.LUCENE_45);// 简单分词器
+		Analyzer analyzer4 = new WhitespaceAnalyzer(Version.LUCENE_45);// 空格分词器
+		String text = "this is my house,I am come form Xian,My email is " + "xxx@qq.com,and my qq is 154625554";
+		AnalyzerUtils.displayToken(text, analyzer1);
+		AnalyzerUtils.displayToken(text, analyzer2);
+		AnalyzerUtils.displayToken(text, analyzer3);
+		AnalyzerUtils.displayToken(text, analyzer4);
+	}
+
+	@Test
+	public void test02() { // 对中文分词不适用
+		Analyzer analyzer1 = new StandardAnalyzer(Version.LUCENE_45);// 标准分词器
+		Analyzer analyzer2 = new StopAnalyzer(Version.LUCENE_45);// 停用分词器
+		Analyzer analyzer3 = new SimpleAnalyzer(Version.LUCENE_45);// 简单分词器
+		Analyzer analyzer4 = new WhitespaceAnalyzer(Version.LUCENE_45);// 空格分词器
+		String text = "西安市雁塔区";
+		AnalyzerUtils.displayToken(text, analyzer1);
+		AnalyzerUtils.displayToken(text, analyzer2);
+		AnalyzerUtils.displayToken(text, analyzer3);
+		AnalyzerUtils.displayToken(text, analyzer4);
+	}
+
+	@Test
+	public void test03() { // 对中文分词不适用
+		Analyzer analyzer1 = new StandardAnalyzer(Version.LUCENE_45);// 标准分词器
+		Analyzer analyzer2 = new StopAnalyzer(Version.LUCENE_45);// 停用分词器
+		Analyzer analyzer3 = new SimpleAnalyzer(Version.LUCENE_45);// 简单分词器
+		Analyzer analyzer4 = new WhitespaceAnalyzer(Version.LUCENE_45);// 空格分词器
+		String text = "how are you thank you";
+		System.out.println("************标准分词器***************");
+		AnalyzerUtils.displayAllTokenInfo(text, analyzer1);
+		System.out.println("************停用分词器***************");
+		AnalyzerUtils.displayAllTokenInfo(text, analyzer2);
+		System.out.println("************简单分词器***************");
+		AnalyzerUtils.displayAllTokenInfo(text, analyzer3);
+		System.out.println("*************空格分词器**************");
+		AnalyzerUtils.displayAllTokenInfo(text, analyzer4);
+	}
+}
